ac6019f63f2984c9cda133a1cbc843daf7ac24b6
Oleksandr Kazimirov <kazimirov.oleksandr@gmail.com>
2026-02-14 11:30:29 +0200

    Release 3.4.0-KZM-1.0-RC1 (test release)

--- tools/docker/build-stateful-functions.sh
+++ tools/docker/build-stateful-functions.sh
@@ -21,7 +21,7 @@ set -e
 # Do not change the name of this variable;
 # it is referenced in the tools/releasing/update_branch_version.sh script
 #
-VERSION_TAG=3.4.0-KZM-1.0-SNAPSHOT
+VERSION_TAG=3.4.0-KZM-1.0-RC1
 
 #
 # setup the environment 
